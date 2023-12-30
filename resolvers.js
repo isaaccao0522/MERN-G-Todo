049d1492f9369d98038f8eb13c11bbdfaa13cf63
Derek Tsao <43665914+isaaccao0522@users.noreply.github.com>
2023-12-30 18:28:20 +0800

    09: deleteTodo, updateTodo are done.

--- resolvers.js
+++ resolvers.js
@@ -16,8 +16,8 @@ const resolvers = {
     },
 
     getTodo: async (parent, args) => {
-      const { id} = args
-      const todo = await Todo.findById( id ).exec ();
+      const { id } = args;
+      const todo = await Todo.findById(id).exec();
       return todo;
     }
   },
@@ -27,8 +27,9 @@ const resolvers = {
       const { title, detail } = args;
       console.log(args);
 
-      if ( title) {
-        throw new Error ( "The todo is existed !!!");
+      const existingTodo = await Todo.findOne ({ title}).exec();
+      if (existingTodo) {
+        throw new Error("The todo is existed !!!");
       }
       const newTodo = new Todo({
         title: title,
@@ -36,7 +37,49 @@ const resolvers = {
       });
       await newTodo.save();
       return newTodo;
-    }
+    },
+
+    deleteTodo: async (parent, args) => {
+      const { id } = args;
+
+      console.log(id);
+
+      if (!id) {
+        throw new Error("The todo's id is required!!!");
+      }
+
+      const todo = await Todo.findById(id).exec();
+      if (!todo) {
+        throw new Error("The todo is not existed !!!");
+      }
+      const deletedTodo = await Todo.findByIdAndDelete(id);
+      return deletedTodo;
+    },
+
+    // deleteAll: async () => {
+    //   await Todo.deleteMany().exec();
+    //   return;
+    // },
+
+    updateTodo: async (parent, args) => {
+      const { id, title, detail } = args;
+
+      console.log(args);
+
+      if (!id) {
+        throw new Error("The todo's id is required!!!");
+      }
+
+      const todo = await Todo.findById(id).exec();
+      if (!todo) {
+        throw new Error("The todo is not existed !!!");
+      }
+      const updateTodo = await Todo.findByIdAndUpdate(id, {
+        title: title,
+        detail: detail
+      });
+      return updateTodo;
+    },
   }
 };
 
--- typeDefs.js
+++ typeDefs.js
@@ -7,6 +7,7 @@ const typeDefs = `#graphql
     title: String,
     detail: String,
     createdAt: Date!
+    updatedAt: Date
   }
 
   type Query {
@@ -18,6 +19,11 @@ const typeDefs = `#graphql
   type Mutation {
     addTodo (
       title: String, detail: String) : Todo
+
+    deleteTodo ( id:ID): Todo
+    # deleteAll:[Todo]
+  
+    updateTodo ( id:ID, title: String, detail: String, updatedAt: Date): Todo
   }
 `;
 
